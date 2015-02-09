12885dc9af05617a77846121ad6e2f36a63f21ae
confusingSmile <confusingSmile@users.noreply.github.com>
2015-02-09 12:47:01 +0100

    Create CommandProcessor.php

---
+++ CommandProcessor.php
@@ -0,0 +1,45 @@
+<?php
+	class CommandProcessor{
+		
+		function CommandProcessor(){
+			
+		}
+		
+		function processCommand($command){
+			$output = "Invalid command";
+			str_replace("/", "" , $command);
+			strip_tags($command);
+			$command = explode(" ", $command);
+			switch($command[0])
+				case "?":
+					showHelp();
+					break;
+				case "down":
+					//output = player->travel(0);
+				case "left":
+					//$output = $player->travel(1);
+					break;
+				case "up":
+					//$output = $player->travel(2);
+					break;
+				case "right":
+					//$output = $player->travel(3);
+					break;
+				case "use":
+					//output = not really a command
+					//doing something with whatever comes after use (expecting item name) 
+					break;
+				case "unlock":
+					//output = $player->getCurrentRoom()->
+					break;
+					
+			return $output;
+		}
+		
+		function showHelp(){
+			return "commands";
+		}
+		
+		
+	}
+?>
