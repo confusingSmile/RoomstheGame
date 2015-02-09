939ce62658571e5b4ef7473b5c2d6ff0af04bfab
confusingSmile <confusingSmile@users.noreply.github.com>
2015-02-09 12:43:34 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<form action="index.php" method="post">
+			<textarea><?php echo $output; ?></textarea>
+			<input type="text" name="input" value="">
+			<button type="submit" value="OK">
+		</form>
+	</body>
+</html>
